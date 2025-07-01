061164c3eff436b2fc35906e0245db515cfc4bfc
Kallam Taylor <kallam_taylor@hotmail.com>
2025-07-01 12:44:49 +0100

    Added - ReadME User Story + Psuedo Code JS

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 #Concentration Memory Game# concentration-memory-game
+
+User Story
+
+As a player, I want to play the memory game so that I can see how high a score I can get from remembering the increasing sequence of the characters movements.
+
+As a player, I want to see a landing page with a colorful graphic and title when I arrive at the website to know I'm in the right place.
+
+As a player, I want to see a clearly labelled start button on the landing page, so I instantly know how to start the game.
+
+As a player, I want to see a visual represenation and audio representation that the game has started, with timer showing game duration.
+
+As a player, I want to easily be able to see the movement and hear the sound of the game characters, so I'm able to know the sequence.
+
+As a player, I want to easily know when the movement and sequence of the characters has ended and when it is my turn to select my game move.
+
+As a player, I want visual and audio feedback while selecting my move so I know my choice has been registered.
+
+As a player, I want visual and audio feedback if while selecting my move, if I've correctly selected the previous sequence shown.
+
+As a player, I want to see a visual score display that increases for every sequence iteration that I've correctly chosen.
+
+As a player, I want visual and audio feedback if while selecting my move, if I've incorrectly selected the previous sequence shown.
+
+As a player, I want to be presented with a clear message when I've lost the game by incorrectly selecting the previous sequence and for my current score to be displayed; with the highest score achieved next to it.
+
+As a player, I want a option (button) to play another round to try and improve my score.
+
---
+++ js/app.js
@@ -0,0 +1,64 @@
+/*--------------------------------- Pseudo ---------------------------------*/
+
+// * Considerations:
+// * Loading Title Screen when page loads.
+// * Eight characters on the screen equally spaced out.
+// * Start button to start the gane.
+// * Character movement with single audio sound assigned to each character.
+// * Display the current score on the screen (id="score-display") (localStorage.setItem("score", "score-display");) (scoreDisplay.textContent = `score: ${score}`;)
+// * Player clicks correct sequence, next turn starts, 100 points to player
+// * Player clicks incorrect sequence, game over, final score and highest score achieved displayed. (`Game over! Your final score is: ${score}`)
+
+// Inputs (user submitted or computer generated)
+// * User will click the start button to start the game.
+// * Computer will move the character/s with a single audio sound attached to that character in a increasing sequence.
+// * Computer will move all characters at once with a specific audio sound attached to show that the sequence for this turn is over.
+// * User will click the character/s that moved in the building sequence correctly or incorrectly. 
+
+// Variables & State (think scores, choices, timers, lives etc)
+// * IF user clicks characters in the sequence correctly, then score will increase by 100.
+// * IF user clicks characters in the sequence correctly, then computer will add one character movement to the sequence.
+// * IF user clicks characters in the sequence incorrectly, then game will end.
+// * IF user clicks characters in the sequence incorrectly, current score & high score will be displayed in center with "Game Over" message.
+// * Timer will show in the top left hand corner for game duration
+// * Timer will stop when the game is over.
+
+// User Interactions (user initiated events like clicks, hovers, key presses etc)
+// * User clicks on the characters in the correct sequence.
+// * User clicks on the characters in the incorrect sequence.
+
+// Core Logic / Rules (List the core rules that will dictate the win lose condition)
+// * Lose condition - player incorrectly inputs turn's sequence
+// * High score condition - player succesfully remembers and inputs correct turn sequence multiple times.
+
+
+// Conditions / Branching (which conditions will lead to which things happening)
+// * Character movement turn to turn increases by one.
+// * Character movement follows a sequence that builds upon the last turn.
+// * Character movement added each turn is randomly chosen via a random number generator.
+// * Scoring - player correctly chosing turns sequence increases score.
+
+
+// Loops (if any) (does any logic repeat? For example a ticking timer or in a game of poker maybe multiple computer choices generated on a loop)
+// * Character movement loop
+// * Player input loop
+// * Increasing timer loop
+// * Scoring and Game Status Loop
+
+// Outputs / Feedback (What will the app output to the screen)
+// * Character movement (computer): each turn characters will move in a sequence increasing by 1 each turn.
+// * Character movement (player): each turn characters will move when player clicks on them.
+// * Score display: indicator of players score on the top left of the screen.
+// * Game over message: indicator of if the player has met the lose condition (incorrect sequence clicked)
+// * Final game score and Highest game score achieved.
+
+
+/*-------------------------------- Constants --------------------------------*/
+
+/*-------------------------------- Variables --------------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
